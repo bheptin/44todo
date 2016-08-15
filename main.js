43395be3b087c1949798f483c9d3a7a05c0467fe
Brian H <bheptin@gmail.com>
2016-08-14 22:11:17 -0400

    Sunday night

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ $(document).ready(function() {
           "Authorization": "Token token=supadupasecret"
         },
         success: (response) => {
-          var todoHTML = response.data.map((todo) => `<li data-id=${todo.id} ${todo.attributes}>
-          ${todo.attributes.input}<input class ="checked" type="checkbox" name="selection">
+          var taskHTML = response.data.map((task) => `<li data-id=${task.id} ${task.attributes.todo}>
+          ${task.attributes.input}<input class ="checked" type="checkbox" name="selection">
           <button>Delete</button></li>`);
 
           ///adding the server info to the todo ul
@@ -23,24 +23,24 @@ $(document).ready(function() {
 
 
       ///getting info from site back to server
-      $.ajax({
-        type: "GET",
-        url: `${site}/todos`,
-        headers: {
-          "Authorization": "Token token=supadupasecret"
-        },
-        ///function to get data back to server
-        success: function (data){
-          $.each( function(){
-            todoHTML.append(`<li data-id=${todo.id} ${todo.attributes}>
-            ${todo.attributes.input} <input class ="checked" type="checkbox" name="selection">
-            <button>Delete</button></li>`);
-          });
+      // $.ajax({
+      //   type: "GET",
+      //   url: `${site}/todos`,
+      //   headers: {
+      //     "Authorization": "Token token=supadupasecret"
+      //   },
+      //   ///function to get data back to server
+      //   success: function (data){
+      //     $.each( function(){
+      //       todoHTML.append(`<li data-id=${task.id} ${task.attributes}>
+      //       ${todo.attributes.input} <input class ="checked" type="checkbox" name="selection">
+      //       <button>Delete</button></li>`);
+      //     });
 
           ////setting form for url
           $("form").submit(function(event){
             $.post({
-              url: `${site}/todo`,
+              url: `${site}/todos`,
               headers: {
                 "Authorization": "Token token=supadupasecret"
               },
@@ -50,13 +50,11 @@ $(document).ready(function() {
               },
 
             });
-            var todoHTML = `<li>${$(this).find("textarea").val()}
-            <input class ="checked" type="checkbox" name="selection">
-            <button>Delete</button></li>`;
-            $("#todo").append(todoHTML);  ////this makes an html skeleton "this" is the FORM
+            var taskHTML = `<li>${$(this).find("input").val()}`;
+            $("#todo").append(taskHTML);  ////this makes an html skeleton "this" is the FORM
 
             event.preventDefault(); ///keeps form data from being lost on button click
-            $("textarea").val("");
+            $("input").val("");
 
           });
 
