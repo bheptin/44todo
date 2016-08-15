176950efd3b97da0b1df68f8a440b90f882bb613
Brian H <bheptin@gmail.com>
2016-08-15 17:16:38 -0400

    monday re-fix

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-    var site = "https://fathomless-woodland-51903.herokuapp.com"
-
+    var site = "https://fathomless-woodland-51903.herokuapp.com";
 
 
     $(document).ready(()=> {
-      ///getting information from server and turning it over to site
+      ///getting information from server and turning it over to site in object notation
       $.getJSON({
         url: `${site}/todos`,
         headers: {
@@ -16,26 +15,26 @@ $(document).ready(function() {
           <button>Delete</button></li>`);
 
           ///adding the server info to the todo ul
-          $("#todo").append(todoHTML.join("\n"));
+          $("#todo").append(taskHTML.join("\n"));
 
         }
       });
 
 
-      ///getting info from site back to server
-      // $.ajax({
-      //   type: "GET",
-      //   url: `${site}/todos`,
-      //   headers: {
-      //     "Authorization": "Token token=supadupasecret"
-      //   },
-      //   ///function to get data back to server
-      //   success: function (data){
-      //     $.each( function(){
-      //       todoHTML.append(`<li data-id=${task.id} ${task.attributes}>
-      //       ${todo.attributes.input} <input class ="checked" type="checkbox" name="selection">
-      //       <button>Delete</button></li>`);
-      //     });
+      ///getting info from site back to server in "realtime"
+      $.ajax({
+        type: "GET",
+        url: `${site}/todos`,
+        headers: {
+          "Authorization": "Token token=supadupasecret"
+        },
+        ///function to get data back to server
+        success: function (data){
+          $.each( function(){
+            taskHTML.append(`<li data-id=${task.id} ${task.attributes}>
+            ${todo.attributes.input} <input class ="checked" type="checkbox" name="selection">
+            <button>Delete</button></li>`);
+          });
 
           ////setting form for url
           $("form").submit(function(event){
@@ -46,7 +45,7 @@ $(document).ready(function() {
               },
               data: $(this).serialize(),  ////parameters
               success: function(response){
-                $("#todo li:last").data(response.attributes.id) ///response=data key///data&id value from server
+                $("#todo li:last").data("id", response.attributes.id) ///response=data key///data&id value from server
               },
 
             });
